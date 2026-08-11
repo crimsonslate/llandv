426691f6da7d5b7b7c33df0bd8b0f38e88fcd3e8
Blake Nall <blake@crimsonslate.com>
2026-08-11 16:02:54 -0500

    Update tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,168 +1,105 @@
-from django.test import Client, TestCase
+import tempfile
+import pytest
+
+from django.test import Client
 from django.urls import reverse
+from portfolio.models import Gallery, Post
 
 
-class HomeViewTestCase(TestCase):
-    def setUp(self):
-        self.location = reverse("portfolio:home")
-        self.client = Client()
-
-    def test_get_allowed(self):
-        """Fails if the response to a GET request is anything other than 200."""
-        response = self.client.get(self.location)
-        self.assertEqual(response.status_code, 200)
-
-    def test_cache_control_header(self):
-        """Fails if ``Cache-Control`` wasn't in the response headers."""
-        response = self.client.get(self.location)
-        self.assertIn("Cache-Control", response.headers.keys())
-
-    def test_vary_hx_request_header(self):
-        """Fails if ``Vary`` with value ``HX-Request`` wasn't in the response headers."""
-        response = self.client.get(self.location)
-        self.assertIn("Vary", response.headers.keys())
-        self.assertIn("HX-Request", response.headers["Vary"])
-
-    def test_full_template_used_on_non_htmx_request(self):
-        """Fails if the full template wasn't rendered on non-htmx request."""
-        expected_template_name = "portfolio/home.html"
-        headers = {"HX-Request": "true", "HX-Boosted": "true"}
-        response = self.client.get(self.location, headers=headers)
-        self.assertTemplateUsed(response, expected_template_name)
-        headers = {}
-        response = self.client.get(self.location, headers=headers)
-        self.assertTemplateUsed(response, expected_template_name)
-
-    def test_main_partial_template_used_on_htmx_request(self):
-        """Fails if the main partial template wasn't rendered on htmx request."""
-        expected_template_name = "main"
-        headers = {"HX-Request": "true", "HX-Boosted": "false"}
-        response = self.client.get(self.location, headers=headers)
-        self.assertTemplateUsed(response, expected_template_name)
+@pytest.fixture
+def client():
+    yield Client()
 
 
-class AboutViewTestCase(TestCase):
-    def setUp(self):
-        self.location = reverse("portfolio:about")
-        self.client = Client()
-
-    def test_get_allowed(self):
-        """Fails if the response to a GET request is anything other than 200."""
-        response = self.client.get(self.location)
-        self.assertEqual(response.status_code, 200)
-
-    def test_cache_control_header(self):
-        """Fails if ``Cache-Control`` wasn't in the response headers."""
-        response = self.client.get(self.location)
-        self.assertIn("Cache-Control", response.headers.keys())
-
-    def test_vary_hx_request_header(self):
-        """Fails if ``Vary`` with value ``HX-Request`` wasn't in the response headers."""
-        response = self.client.get(self.location)
-        self.assertIn("Vary", response.headers.keys())
-        self.assertIn(response.headers["Vary"], "HX-Request")
-
-    def test_full_template_used_on_non_htmx_request(self):
-        """Fails if the full template wasn't rendered on non-htmx request."""
-        expected_template_name = "portfolio/about.html"
-        headers = {"HX-Request": "true", "HX-Boosted": "true"}
-        response = self.client.get(self.location, headers=headers)
-        self.assertTemplateUsed(response, expected_template_name)
-        headers = {}
-        response = self.client.get(self.location, headers=headers)
-        self.assertTemplateUsed(response, expected_template_name)
-
-    def test_main_partial_template_used_on_htmx_request(self):
-        """Fails if the main partial template wasn't rendered on htmx request."""
-        expected_template_name = "main"
-        headers = {"HX-Request": "true", "HX-Boosted": "false"}
-        response = self.client.get(self.location, headers=headers)
-        self.assertTemplateUsed(response, expected_template_name)
+@pytest.fixture(autouse=True)
+def gallery():
+    yield Gallery.objects.create(pk=1, name="Gallery 1", slug="gallery-1")
 
 
-class ContactViewTestCase(TestCase):
-    def setUp(self):
-        self.location = reverse("portfolio:contact")
-        self.client = Client()
-
-    def test_get_allowed(self):
-        """Fails if the response to a GET request is anything other than 200."""
-        response = self.client.get(self.location)
-        self.assertEqual(response.status_code, 200)
-
-    def test_cache_control_header(self):
-        """Fails if ``Cache-Control`` wasn't in the response headers."""
-        response = self.client.get(self.location)
-        self.assertIn("Cache-Control", response.headers.keys())
-
-    def test_vary_hx_request_header(self):
-        """Fails if ``Vary`` with value ``HX-Request`` wasn't in the response headers."""
-        response = self.client.get(self.location)
-        self.assertIn("Vary", response.headers.keys())
-        self.assertIn("HX-Request", response.headers["Vary"])
-
-    def test_full_template_used_on_non_htmx_request(self):
-        """Fails if the full template wasn't rendered on non-htmx request."""
-        expected_template_name = "portfolio/contact.html"
-        headers = {"HX-Request": "true", "HX-Boosted": "true"}
-        response = self.client.get(self.location, headers=headers)
-        self.assertTemplateUsed(response, expected_template_name)
-        headers = {}
-        response = self.client.get(self.location, headers=headers)
-        self.assertTemplateUsed(response, expected_template_name)
-
-    def test_main_partial_template_used_on_htmx_request(self):
-        """Fails if the main partial template wasn't rendered on htmx request."""
-        expected_template_name = "main"
-        headers = {"HX-Request": "true", "HX-Boosted": "false"}
-        response = self.client.get(self.location, headers=headers)
-        self.assertTemplateUsed(response, expected_template_name)
-
-    def test_form_in_context(self):
-        """Fails if ``form`` wasn't in the response context."""
-        response = self.client.get(self.location)
-        self.assertIn("form", response.context)
+@pytest.fixture(autouse=True)
+def post(gallery):
+    yield Post.objects.create(
+        gallery=gallery, file=tempfile.NamedTemporaryFile(suffix=".jpg").name
+    )
 
 
-class ContactFormViewTestCase(TestCase):
-    def setUp(self):
-        self.location = reverse("portfolio:contact form")
-        self.client = Client()
+@pytest.mark.parametrize(
+    "path",
+    [
+        reverse("portfolio:home"),
+        reverse("portfolio:about"),
+        reverse("portfolio:contact"),
+        reverse("portfolio:gallery list"),
+        reverse(
+            "portfolio:gallery detail",
+            kwargs={"gallery_pk": 1, "gallery_slug": "gallery-1"},
+        ),
+        reverse(
+            "portfolio:post detail",
+            kwargs={
+                "gallery_pk": 1,
+                "gallery_slug": "gallery-1",
+                "post_pk": 1,
+            },
+        ),
+    ],
+)
+@pytest.mark.django_db
+def test_get_allowed(client, path):
+    response = client.get(path)
+    assert response.status_code == 200
 
-    def test_get_allowed(self):
-        """Fails if the response to a GET request is anything other than 200."""
-        response = self.client.get(self.location)
-        self.assertEqual(response.status_code, 200)
 
-    def test_post_allowed(self):
-        """Fails if the response to a POST request is anything other than 200."""
-        response = self.client.post(self.location)
-        self.assertEqual(response.status_code, 200)
+@pytest.mark.parametrize(
+    "path",
+    [
+        reverse("portfolio:home"),
+        reverse("portfolio:about"),
+        reverse("portfolio:contact"),
+        reverse("portfolio:gallery list"),
+        reverse(
+            "portfolio:gallery detail",
+            kwargs={"gallery_pk": 1, "gallery_slug": "gallery-1"},
+        ),
+        reverse(
+            "portfolio:post detail",
+            kwargs={
+                "gallery_pk": 1,
+                "gallery_slug": "gallery-1",
+                "post_pk": 1,
+            },
+        ),
+    ],
+)
+@pytest.mark.django_db
+def test_cache_control_header(client, path):
+    response = client.get(path)
+    assert response.has_header("Cache-Control")
 
-    def test_cache_control_header(self):
-        """Fails if ``Cache-Control`` wasn't in the response headers."""
-        response = self.client.get(self.location)
-        self.assertIn("Cache-Control", response.headers.keys())
 
-    def test_vary_hx_request_header(self):
-        """Fails if ``Vary`` with value ``HX-Request`` wasn't in the response headers."""
-        response = self.client.get(self.location)
-        self.assertIn("Vary", response.headers.keys())
-        self.assertIn("HX-Request", response.headers["Vary"])
-
-    def test_full_template_used_on_non_htmx_request(self):
-        """Fails if the full template wasn't rendered on non-htmx request."""
-        expected_template_name = "portfolio/contact_form.html"
-        headers = {"HX-Request": "true", "HX-Boosted": "true"}
-        response = self.client.get(self.location, headers=headers)
-        self.assertTemplateUsed(response, expected_template_name)
-        headers = {}
-        response = self.client.get(self.location, headers=headers)
-        self.assertTemplateUsed(response, expected_template_name)
-
-    def test_main_partial_template_used_on_htmx_request(self):
-        """Fails if the main partial template wasn't rendered on htmx request."""
-        expected_template_name = "main"
-        headers = {"HX-Request": "true", "HX-Boosted": "false"}
-        response = self.client.get(self.location, headers=headers)
-        self.assertTemplateUsed(response, expected_template_name)
+@pytest.mark.parametrize(
+    "path",
+    [
+        reverse("portfolio:home"),
+        reverse("portfolio:about"),
+        reverse("portfolio:contact"),
+        reverse("portfolio:gallery list"),
+        reverse(
+            "portfolio:gallery detail",
+            kwargs={"gallery_pk": 1, "gallery_slug": "gallery-1"},
+        ),
+        reverse(
+            "portfolio:post detail",
+            kwargs={
+                "gallery_pk": 1,
+                "gallery_slug": "gallery-1",
+                "post_pk": 1,
+            },
+        ),
+    ],
+)
+@pytest.mark.django_db
+def test_vary_on_hx_request_header(client, path):
+    response = client.get(path)
+    assert response.has_header("Vary")
+    assert "HX-Request" in response.headers["Vary"]
