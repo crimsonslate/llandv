bb564f5cd01ee98d25c127e538255edef9659b0e
Blake Nall <blake@crimsonslate.com>
2026-09-21 17:52:31 -0500

    Update templates

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,16 +1,25 @@
-from django.db import models
-from django.utils.translation import gettext_lazy as _
 from datetime import datetime
 
-from wagtail.models import Page
-from wagtail.images.blocks import ImageBlock
-from wagtail.blocks import ListBlock
-from wagtail.admin.panels import FieldPanel, MultiFieldPanel
-from wagtail.fields import StreamField, RichTextField
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from modelcluster.fields import ParentalKey
+from wagtail.contrib.forms.forms import FormBuilder
+from wagtail.contrib.forms.panels import FormSubmissionsPanel
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
+from wagtail.admin.panels import (
+    FieldPanel,
+    MultiFieldPanel,
+    InlinePanel,
+    FieldRowPanel,
+)
+from wagtail.blocks import ListBlock, RichTextBlock
 from wagtail.contrib.settings.models import (
     register_setting,
     BaseGenericSetting,
 )
+from wagtail.fields import StreamField, RichTextField
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.models import Page
 
 
 @register_setting
@@ -47,9 +56,14 @@ class HomePage(Page):
         FieldPanel("hero_image"),
     ]
 
+    def get_context(self, request, *args, **kwargs) -> dict:
+        context = super().get_context(request, *args, **kwargs)
+        context["latest_galleries"] = []
+        return context
 
-class AboutPage(Page):
-    body = RichTextField()
+
+class StandardPage(Page):
+    body = RichTextField(features=["h2", "bold", "italic", "link", "image"])
 
     content_panels = Page.content_panels + [FieldPanel("body")]
 
@@ -59,20 +73,96 @@ class AboutPage(Page):
 
 
 class GalleryIndexPage(Page):
-    pass
+    featured_gallery = models.ForeignKey(
+        "portfolio.GalleryPage",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
+    content_panels = Page.content_panels + [FieldPanel("featured_gallery")]
+
+    promote_panels = [
+        MultiFieldPanel(Page.promote_panels, "Common page configuration")
+    ]
+
+    def get_context(self, request, *args, **kwargs) -> dict:
+        context = super().get_context(request, *args, **kwargs)
+        context["galleries"] = GalleryPage.objects.child_of(self).live()
+        return context
 
 
 class GalleryPage(Page):
     parent_page_types = ["portfolio.GalleryIndexPage"]
 
-    pub_date = models.DateTimeField(default=datetime.now)
-    body = StreamField([("carousel", ListBlock(ImageBlock()))], blank=True)
+    pub_date = models.DateField(default=datetime.today)
+    body = StreamField(
+        [
+            ("description", RichTextBlock(blank=True)),
+            ("carousel", ListBlock(ImageChooserBlock(), max_num=24)),
+        ],
+        blank=True,
+        block_counts={
+            "description": {"max_num": 1},
+            "carousel": {"max_num": 1},
+        },
+    )
 
     content_panels = Page.content_panels + [
         FieldPanel("pub_date"),
         FieldPanel("body"),
     ]
 
+    promote_panels = [
+        MultiFieldPanel(Page.promote_panels, "Common page configuration")
+    ]
 
-class ContactPage(Page):
-    pass
+    @property
+    def thumbnail(self):
+        carousel = self.body.blocks_by_name("carousel")[0]
+        return carousel.value[0] if carousel.value else None
+
+
+class FormField(AbstractFormField):
+    page = ParentalKey(
+        "FormPage", on_delete=models.CASCADE, related_name="form_fields"
+    )
+    placeholder = models.CharField(blank=True, max_length=256)
+
+    panels = AbstractFormField.panels + [FieldPanel("placeholder")]
+
+
+class PortfolioFormBuilder(FormBuilder):
+    def get_create_field_function(self, type):
+        create_field_function = super().get_create_field_function(type)
+
+        def wrapped_create_field_function(field, options):
+            created_field = create_field_function(field, options)
+            created_field.widget.attrs.update(
+                {"placeholder": field.placeholder}
+            )
+            return created_field
+
+        return wrapped_create_field_function
+
+
+class FormPage(AbstractEmailForm):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FormSubmissionsPanel(),
+        FieldPanel("intro"),
+        InlinePanel("form_fields"),
+        FieldPanel("thank_you_text"),
+        MultiFieldPanel(
+            [
+                FieldRowPanel(
+                    [FieldPanel("from_address"), FieldPanel("to_address")]
+                ),
+                FieldPanel("subject"),
+            ],
+            "Email",
+        ),
+    ]
